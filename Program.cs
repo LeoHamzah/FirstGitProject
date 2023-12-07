e7732f0e7ca20f79ef7391ca44a5c615ae8eaff0
LeoHamzah <hamzaabdulraheem247@gmail.com>
2023-12-07 13:28:18 +0100

    My first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Enter a number : ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter a number : ");
+int b = int.Parse(Console.ReadLine());
+System.Console.WriteLine($"The sum of both numbers is {a+b}");
