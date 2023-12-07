8399f59c510d715b5a921508b8f3ea1912c169b8
LeoHamzah <hamzaabdulraheem247@gmail.com>
2023-12-07 15:23:38 +0100

    Made some changes on my initial commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Enter a number : ");
-int a = int.Parse(Console.ReadLine());
-Console.Write("Enter a number : ");
-int b = int.Parse(Console.ReadLine());
-System.Console.WriteLine($"The sum of both numbers is {a+b}");
+﻿void SumOfNumbers()
+{
+    Console.Write("Enter a number : ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Enter a number : ");
+    int b = int.Parse(Console.ReadLine());
+    System.Console.WriteLine($"The sum of both numbers is {a+b}");
+}
